a0fb8efc2e2b8be7e106646d7e46a59e2fd80b73
Aryanand9 <aryanand0008@gmail.com>
2023-08-06 18:26:30 +0530

    after sidebar

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,12 +1,11 @@
-import { UserButton } from '@clerk/nextjs'
-import React from 'react'
+import { SearchInput } from "@/components/SearchInput";
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
 
 const Page = () => {
-  return (
-    <div>
-      <UserButton afterSignOutUrl='/'/>
-    </div>
-  )
-}
+  return <div className="h-full p-4 space-y-2">
+    <SearchInput/>
+  </div>;
+};
 
-export default Page
+export default Page;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,36 @@
 'use client'
-import {Menu} from 'lucide-react'
+import { cn } from '@/lib/utils'
+import { UserButton } from '@clerk/nextjs'
+import {Menu, Sparkles} from 'lucide-react'
+import { Poppins } from 'next/font/google'
+import Link from 'next/link'
+import { Button } from './ui/button'
+import { ModeToggle } from './ui/mode-toggle'
+import { MobileSidebar } from './MobileSidebar'
+
+const font= Poppins({
+    weight:'600',
+    subsets:['latin']
+})
+
 export const Navbar=()=>{
     return(
-        <div className="fixed w-full z-50 flex justify-between items-center py-2 px-2 border-b border-primary/10 bg-secondary">
+        <div className="fixed w-full z-50 flex justify-between items-center py-2 px-2 border-b h-16 border-primary/10 bg-secondary">
             <div className="flex items-start">
-                <Menu className='block md:hidden'/>
+                <MobileSidebar/>
+                                <Link href={'/'}>
+                    <h1 className={cn('hidden md:block text-xl md:text-3xl font-bold text-primary',font.className)}>
+                        companion.ai
+                    </h1>
+                </Link>
+            </div>
+            <div className='flex items-center gap-x-3'>
+                <Button variant={'premium'} size={'sm'}>
+                    Upgrade
+                    <Sparkles className='ml-2'/>
+                </Button>
+                <ModeToggle/>
+                <UserButton/>
             </div>
         </div>
     )
